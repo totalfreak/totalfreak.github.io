4c088cce422bc62ef4a4f6c951df390b2d8dff3c
totalfreak <dgkartin@live.dk>
2015-10-08 16:22:03 +0200

    Yay

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-$('#selectEpisode').change(function () {
-            location.href = 'http://kissanime.com/Anime/One-Piece/' + $(this).val();
-        });
+$(document).ready(function () {
+	
+	$('#selectEpisode').change(function () {
+		'http://kissanime.com/Anime/One-Piece/' + $(this).val()
+        var newVid = "anime/AnimeUpload.com_One_Piece_" + $(this).val() + "_x264.mp4";
+		$("#vid").attr("src", newVid)
+		$("#vid").load();
+    });
+});
