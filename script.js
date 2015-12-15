45b30c782dd562f5db0e7b0dff49f1c445b64a05
Hax3r <hax3r@localhost.localdomain>
2015-12-15 18:07:31 +0100

    Nigger

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	
-	$('#selectEpisode').change(function () {
-		'http://kissanime.com/Anime/One-Piece/' + $(this).val()
-        var newVid = "anime/AnimeUpload.com_One_Piece_" + $(this).val() + "_x264.mp4";
-		$("#vid").attr("src", newVid)
-		$("#vid").load();
-    });
-});
