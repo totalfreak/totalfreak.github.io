bf1ec5fe1a7088f3af812c221ffd61c52a4f3f69
totalfreak <dgkartin@live.dk>
2016-02-10 22:38:11 +0100

    modified:   index.html 	modified:   script.js 	modified:   stylesheet.css 	README.md 	arrow.png 	arrow2.png 	arrow3.png 	arrow4.png 	desktop.ini 	jquery.transform2d.js

--- script.js
+++ script.js
@@ -1,37 +1,139 @@
-$(function() {
-  $(document).on("scroll", onScroll);
+$(document).ready(function(){
+timeout();
+//Navbar showing and hiding
+  $("#arrow").css({transform: "rotate(180deg)"});
+  $("#arrow").toggle(function() {
+      $("#arrow").animate({
+        top: "0",
+        opacity: "0.6",
+        transform: "rotate(360deg)"
+      }, 600, 'easeInOutBack');
+      $(".navbar").animate({
+        top: '-46px',
+        opacity: '0.6'
+      }, 600, 'easeInOutBack');
+      $("#loginPop").animate({
+        top: '2px'
+      }, 600, 'easeInOutBack');
+    }, function() {
+        $("#arrow").animate({
+          top: "42px",
+          opacity: "1",
+          transform: "rotate(180deg)"
+        }, 600, 'easeInOutBack');
+        $(".navbar").animate({
+          top: '-6px',
+          opacity: '1'
+        }, 600, 'easeInOutBack');
+        $("#loginPop").animate({
+          top: '44px'
+        }, 600, 'easeInOutBack');
+      });
 
-  //smoothscroll
-  $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    $(document).off("scroll");
+      setTimeout(function() {
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 1000);
 
-    $('a').each(function() {
-      $(this).removeClass('active');
-    })
-    $(this).addClass('active');
-
-    var target = this.hash,
-      menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top + 2
-    }, 500, 'swing', function() {
-      window.location.hash = target;
-      $(document).on("scroll", onScroll);
-    });
+  //Loading text changer
+    function timeout() {
+    setTimeout(function () {
+      setTimeout(function() {
+        var text = $("h1");
+          $("h1").text("Loading.  ");
+      }, 500);
+      setTimeout(function() {
+        $("h1").text("Loading . ");
+      }, 1000);
+        timeout();
+    }, 1000);
+}
+//Account hiding and stuff
+  $("#login").toggle(function() {
+    $("#loginPop").animate({
+      right: "5px",
+      opacity: "1"
+    }, 600, 'easeOutBack');
+    $("body").addClass("out");
+  }, function() {
+      $("#loginPop").animate({
+        right: "-310px",
+        opacity: "0"
+      }, 600, 'easeInOutBack');
+      $("body").removeClass("out");
   });
-});
-function onScroll(event) {
-  var scrollPos = $(document).scrollTop();
-  $('.topnavLink').each(function() {
-    var currLink = $(this);
-    var refElement = $(currLink.attr("href"));
-    if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-      $('.topnavLink').removeClass("active");
-      currLink.addClass("active");
+
+  //Changing between login and create screen
+  $("#createUser").click(function() {
+    //Switching out login with create screen
+    $("#loginBox").animate({
+      left: "310px",
+      opacity: "0"
+    }, 250, "easeInOutBack");
+    $("#createBox").animate({
+      left: "0",
+      opacity: "1"
+    }, 250, "easeInOutBack");
+  });
+  $("#loginUser").click(function() {
+    //Switching out create with login screen
+    $("#loginBox").animate({
+      left: "0",
+      opacity: "1"
+    }, 250, "easeInOutBack");
+    $("#createBox").animate({
+      left: "-310px",
+      opacity: "0"
+    }, 250, "easeInOutBack");
+  });
+
+//Firebase stuff
+var ref = new Firebase("https://it-eksamen.firebaseio.com/");
+var usersRef = ref.child("users");
+//Firebase create account
+$("#createButton").click(function() {
+  if($("#userLogin").val() != "" && $("#passLogin").val() != "") {
+  var username = $("#userLogin").val();
+  var password = $("#passLogin").val();
+  usersRef.createUser({
+    email: username,
+    password: password
+  }, function(error, userData) {
+    if(error) {
+      console.log("Error creating user: ", error);
     } else {
-      currLink.removeClass("active");
+      console.log("Successfully created account with uid: ", userData.uid);
+      $("#username").val(username);
+      $("#password").val(password);
+      $("#userLogin").val("");
+      $("#passLogin").val("");
+      $("#loginBox").animate({
+        left: "0",
+        opacity: "1"
+      }, 250, "easeInOutBack");
+      $("#createBox").animate({
+        left: "-310px",
+        opacity: "0"
+      }, 250, "easeInOutBack");
     }
   });
+
 }
+});
+//Firebase login account
+$("#loginButton").click(function() {
+  if($("#username").val() != "" && $("#password").val() != "") {
+    usersRef.authWithPassword({
+      email: $("#username").val(),
+      password: $("#password").val()
+    }, function(error, authData) {
+      if(error) {
+        console.log("Login Failed!", error);
+        $("#loginPop").animate({right: "15px"}, 600);
+      } else {
+        console.log("Authenticated Successfully: ", authData);
+      }
+    });
+  }
+});
+});
