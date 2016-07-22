f121912611fbfef74954a6e5fa270f1fbf7e3952
totalfreak <dgkartin@live.dk>
2016-07-22 17:35:21 +0200

    Converted coffeescript to javascript, and made it fit size of window

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 $(function() {
-  /*var width = $(window).width();
-  if(width >= 700) {
-    $("#seen-canvas").remove();
-  }*/
   function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
@@ -48,5 +44,44 @@ function initializeClock(id, endtime) {
 var deadline = new Date("2016/11/16 23:59:59");
 initializeClock('clockdiv', deadline);
 
+//3D wavy background down here
+var width = $(window).width();
+var height = $(window).height();
+//Getting new size values, if window is resized
+window.onresize = function(event) {
+  var width = $(window).width();
+  var height = $(window).height();
+};
+  var context, equilateralAltitude, height, noiser, patch_height, patch_width, scene, shape, t, triangleScale, width;
+  width = width;
+  height = height;
+  equilateralAltitude = Math.sqrt(3.0) / 2.0;
+  triangleScale = 70;
+  patch_width = width * 1.5;
+  patch_height = height * 1.5;
+  shape = seen.Shapes.patch(patch_width / triangleScale / equilateralAltitude, patch_height / triangleScale).scale(triangleScale).translate(-patch_width / 2, -patch_height / 2 + 80).rotx(-0.3);
+  seen.Colors.randomSurfaces2(shape);
+  scene = new seen.Scene({
+    model: seen.Models["default"]().add(shape),
+    viewport: seen.Viewports.center(width, height)
+  });
+context = seen.Context('seen-canvas', scene).render();
+t = 0;
+noiser = new seen.Simplex3D(shape);
+context.animate().onBefore(function(t) {
+var i, j, len, len1, p, ref, ref1, results, surf;
+ref = shape.surfaces;
+results = [];
+for (i = 0, len = ref.length; i < len; i++) {
+  surf = ref[i];
+  ref1 = surf.points;
+  for (j = 0, len1 = ref1.length; j < len1; j++) {
+    p = ref1[j];
+    p.z = 4 * noiser.noise(p.x / 8, p.y / 8, t * 1e-4);
+  }
+  results.push(surf.dirty = true);
+}
+return results;
+}).start();
 
 });
