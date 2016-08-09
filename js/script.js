19bbe7bf99d2fb3fed8e5933a3da6d82834cb012
totalfreak <dgkartin@live.dk>
2016-08-09 22:11:39 +0200

    test meta-wiew

--- js/script.js
+++ js/script.js
@@ -72,7 +72,7 @@ document.onresize = function(event) {
 };
 if(width < 990) {
   triangleScale = 180;
-  $("#testText2").css({paddingTop: 30, paddingBottom: 30, width: 500, fontSize: "3em"});
+  $("#testText2").css({paddingTop: 30, paddingBottom: 30, width: 500, fontSize: "3em", borderRadius: 75});
 } else {
   triangleScale = 120;
 }
